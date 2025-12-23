503074fda8958d919577f9c12b1e5f4da371fa95
Lucas Adrian Sebastian Casas <lucas.a.s.casas@outlook.es>
2025-12-23 11:06:51 -0300

    feat: implement Person class with registry and relationship linking

--- app/main.py
+++ app/main.py
@@ -1,8 +1,37 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        """
+        It represents a person and manages a centralized registry of instances.
+
+        Class attributes:
+            people (dict): Dictionary that stores all created instances,
+            using the name as the key and the Person object as the value.
+
+        Class method:
+            Initializes a new instance of Person and registers
+            it 8n the class dictionary.
+        """
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    """
+    Convert dictionaries into Person instances and link their relationships.
+
+    Create the objects in the first pass and in the second pass
+    establish the "wife" / "husband" attributes, connecting them to each other.
+    """
+    for p in people:
+        Person(p["name"], p["age"])
+    for p in people:
+        individual = Person.people[p["name"]]
+        for relation in p:
+            if relation in ["wife", "husband"] and p[relation] is not None:
+                partner_name = p.get(relation)
+                partner_instance = Person.people.get(partner_name)
+                setattr(individual, relation, partner_instance)
+    return [Person.people[p["name"]] for p in people]
